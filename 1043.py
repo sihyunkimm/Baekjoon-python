403e01186e2b1c651b98d5ac543feccf722305bb
sihyunkimm <sihyunkim2654@gmail.com>
2025-12-23 23:49:39 +0900

    solve: 1043

---
+++ 1043.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+knowList = set(input().split()[1:])
+parties = []
+
+for _ in range(m):
+    parties.append(set(input().split()[1:]))
+
+for _ in range(m):
+    for party in parties:
+        if party & knowList:
+            knowList = knowList.union(party)
+
+answer = 0
+for party in parties:
+    if not party & knowList:
+        answer += 1
+    
+print(answer)
